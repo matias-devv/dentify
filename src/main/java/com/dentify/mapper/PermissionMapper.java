fb09d5a654f3fb50e99e4b013ca172ef04ce2488
matias-devv <rmatias.dev@gmail.com>
2026-03-04 21:31:36 -0300

    feat(mapper): add PermissionMapper to centralize Permission entity construction

--- src/main/java/com/dentify/mapper/PermissionMapper.java
+++ src/main/java/com/dentify/mapper/PermissionMapper.java
@@ -1,4 +1,14 @@
 package com.dentify.mapper;
 
+import com.dentify.security.model.Permission;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PermissionMapper {
+
+    public Permission buildPermission(String name) {
+        return Permission.builder()
+                        .permissionName(name)
+                        .build();
+    }
 }
