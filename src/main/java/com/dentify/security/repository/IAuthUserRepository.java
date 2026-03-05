b7e5c3bde77fd503dc2559c20a4eeb6b4d280012
matias-devv <rmatias.dev@gmail.com>
2026-03-04 21:46:31 -0300

    feat(auth): add query to fetch AuthUser with UserProfile and Clinic by username

--- src/main/java/com/dentify/security/repository/IAuthUserRepository.java
+++ src/main/java/com/dentify/security/repository/IAuthUserRepository.java
@@ -1,4 +1,27 @@
 package com.dentify.security.repository;
 
-public interface IAuthUserRepository {
+import com.dentify.security.model.AuthUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IAuthUserRepository extends JpaRepository<AuthUser, Long> {
+
+    @Query("SELECT au FROM AuthUser au LEFT JOIN FETCH au.roles WHERE au.username = :username")
+    Optional<AuthUser> findByUsernameWithRoles(@Param("username") String username);
+
+    boolean existsByUsername(String email);
+
+    @Query("""
+       SELECT au
+       FROM AuthUser au
+       LEFT JOIN FETCH au.userProfile up
+       LEFT JOIN FETCH up.clinic
+       WHERE au.username = :username
+       """)
+    Optional<AuthUser> findByUsernameWithProfileAndClinic(@Param("username") String username);
 }
