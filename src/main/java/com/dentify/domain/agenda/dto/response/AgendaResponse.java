d912ce7d70569605e093590132da93b6e0e37ff1
matias-devv <rmatias.dev@gmail.com>
2026-06-24 19:51:18 -0300

    refactor(agenda): rename AgendaResponse fields for clarity

--- src/main/java/com/dentify/domain/agenda/dto/response/AgendaResponse.java
+++ src/main/java/com/dentify/domain/agenda/dto/response/AgendaResponse.java
@@ -1,5 +1,5 @@
 package com.dentify.domain.agenda.dto.response;
 
-public record AgendaResponse(Long id,
-                             String name) {
+public record AgendaResponse(Long id_agenda,
+                             String agenda_name) {
 }
