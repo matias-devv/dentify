dcaaad6b7ed2a0267631d3b5da0ddb26845eafc6
matias-devv <rmatias.dev@gmail.com>
2026-06-25 21:15:56 -0300

    feat(clinic): add tenant identifier to clinic entity

--- src/main/java/com/dentify/domain/clinic/model/Clinic.java
+++ src/main/java/com/dentify/domain/clinic/model/Clinic.java
@@ -1,9 +1,10 @@
-package com.dentify.domain.clinic;
+package com.dentify.domain.clinic.model;
 
 
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.TenantId;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
@@ -19,6 +20,7 @@ public class Clinic {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_clinic")
     private Long id;
 
     @Column(nullable = false, length = 100)
@@ -38,6 +40,9 @@ public class Clinic {
 
     @Column(nullable = false)
     private boolean active = true;
+
+    @Column(name = "tenant_id", nullable = false, unique = true)
+    private String tenantId;
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
